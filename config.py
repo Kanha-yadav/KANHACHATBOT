bf8f1f5e02e2a330636cf201cdb5fb0183046e23
Kanha-yadav <163987369+Kanha-yadav@users.noreply.github.com>
2024-03-30 17:22:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "20408169"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "1e641df4a353853622d07119d05c4fbc"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6664582540"
-SUPPORT_GRP = "HEROKUFREECC"
-UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "iam_daxx"
+OWNER_ID = "6287955426"
+SUPPORT_GRP = "ZX_LEGEND_TEAM"
+UPDATE_CHNL = "UR_AMAN_XD"
+OWNER_USERNAME = "Zx_Daddy_CallinG"
 
